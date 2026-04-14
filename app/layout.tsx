2444cb397c335a19a14d44f5b8c3eb8f477fa2fa
IssaKlassik <issaklassik@IssaKlassiks-MacBook-Air.local>
2026-04-14 08:23:19 +0200

    fix: explicit viewport string and removing w-screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,14 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { ThemeProvider } from "next-themes";
 
 import "./globals.css";
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
 
 export const metadata: Metadata = {
   title: "CommitForge",
